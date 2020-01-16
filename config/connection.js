80a628ef79590b5e1da4884e72fa39e19b40de28
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-01-16 17:37:15 -0500

    set up connection correctly

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,22 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
 host: "localhost",
-
 // Your port; if not 3306
 port: 3306,
-
 // Your username
 user: "root",
 // Your password
 password: "uprcayey",
 database: ""
+});
+
+// Make connection.
+connection.connect(function(err) {
+if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+}
+console.log('connected as id ' + connection.threadId);
 });
 
 // Export connection for our ORM to use.
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.set(express.static("public"));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
