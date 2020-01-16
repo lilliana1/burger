5380c762289c20778e5e6a68b26491cd04dd2d0b
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-01-15 19:29:38 -0500

    added folders and started with main.handlebars and connected server without db

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+host: "localhost",
+
+// Your port; if not 3306
+port: 3306,
+
+// Your username
+user: "root",
+// Your password
+password: "uprcayey",
+database: ""
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
